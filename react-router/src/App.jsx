6947b3d9d747b52d4836115c872e0c777e89a079
JT4848 <jtpricone@gmail.com>
2023-07-19 00:09:10 -0400

    initial commit

---
+++ react-router/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+const App = () => {
+  
+
+  return (
+    <>
+      <div id="container">
+      
+      <div id="navbar">{/* navigation here */}</div>
+      <div id="main-section">{/* routes here */}</div>
+    </div>
+    </>
+  )
+}
+
+export default App
